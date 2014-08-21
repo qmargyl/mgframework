4511e0cde10fb2a8b07f6a62996214d748ddbad1
unknown <mg@qmargyl.com>
2014-08-21 12:24:23 +0200

    MGComponent class test updates

--- src/mgframework/class_test/test_MGComponent.cpp
+++ src/mgframework/class_test/test_MGComponent.cpp
@@ -8,5 +8,36 @@ void MGClassTester::test_MGComponent()
 
 	// Test that two consecutively created components have consecutive IDs. 
 	ASSERT_NOT_EQUAL(comp1.getID() + 1, comp2.getID(), "MGComponent ID not increased properly");
+	
+	// Logging ON/OFF
+	ASSERT_NOT_EQUAL(comp1.loggingEnabled(), false, "MGComponent not created with disabled logging");
+	comp1.enableLogging();
+	ASSERT_NOT_EQUAL(comp1.loggingEnabled(), true, "MGComponent not able to enable logging");
+	comp1.disableLogging();
+	ASSERT_NOT_EQUAL(comp1.loggingEnabled(), false, "MGComponent not able to disable logging");
+	comp1.enableLogging();
+	ASSERT_NOT_EQUAL(comp1.loggingEnabled(), true, "MGComponent not able to enable logging");
+	comp1.disableLogging();
+	ASSERT_NOT_EQUAL(comp1.loggingEnabled(), false, "MGComponent not able to disable logging");
 
+	// Owner
+	ASSERT_NOT_EQUAL(comp1.isOwned(), false, "MGComponent not created without owner");
+	comp1.setOwner(4);
+	ASSERT_NOT_EQUAL(comp1.isOwned(), true, "MGComponent not able to set owner");
+	ASSERT_NOT_EQUAL(comp1.getOwner(), 4, "MGComponent not able to set owner");
+	comp1.setOwner(0);
+	ASSERT_NOT_EQUAL(comp1.isOwned(), false, "MGComponent not able to unset owner");
+	ASSERT_NOT_EQUAL(comp1.getOwner(), 0, "MGComponent not able to unset owner");
+	
+	// toString(bool)
+	ASSERT_NOT_EQUAL(MGComponent::toString(true), std::string("true"), "toString(bool) returned incorrectly");
+	ASSERT_NOT_EQUAL(MGComponent::toString(false), std::string("false"), "toString(bool) returned incorrectly");
+	ASSERT_NOT_EQUAL(comp1.toString(true), std::string("true"), "toString(bool) returned incorrectly");
+	ASSERT_NOT_EQUAL(comp1.toString(false), std::string("false"), "toString(bool) returned incorrectly");
+	
+	// toString(int)
+	ASSERT_NOT_EQUAL(MGComponent::toString(17), std::string("17"), "toString(int) returned incorrectly");
+	ASSERT_NOT_EQUAL(MGComponent::toString(-17), std::string("-17"), "toString(int) returned incorrectly");
+	ASSERT_NOT_EQUAL(comp1.toString(17), std::string("17"), "toString(int) returned incorrectly");
+	ASSERT_NOT_EQUAL(comp1.toString(-17), std::string("-17"), "toString(int) returned incorrectly");
 }
