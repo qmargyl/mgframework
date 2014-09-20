e54f27f62b5ff033c0dd7d834bf09a3eb5572497
Martin Gyllensten <martin.gyllensten@gmail.com>
2014-09-20 07:40:09 +0200

    Fixed last commits build error

--- src/mgframework/stubs/mgframeworkstub.cpp
+++ src/mgframework/stubs/mgframeworkstub.cpp
@@ -1,11 +1,6 @@
 #include "mgframeworkstub.h"
 #include "../mgpathitem.h"
 
-MGFrameworkStub::MGFrameworkStub()
-{
-
-}
-
 bool MGFrameworkStub::init(int w, int h, int tw, int th)
 {
 	m_Map.init(w, h, tw, th, 1024, 768);
